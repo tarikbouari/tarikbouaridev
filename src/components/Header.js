e616de5729c9b8e716489743c7ac255d679f1beb
tarikbouari <tarikbouari@gmail.com>
2025-11-16 00:56:09 +0100

    Ajust animate etext

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import { Link } from 'react-scroll';
 
 const Header = () => (
   <div className="flex justify-center items-center h-[100vh] md:h-[100vh] xl:h-[100vh] bnbg" id="home">
-    <div className=" mx-auto  container px-6 py-8 ">
+    <div className=" mx-auto  container px-6 py-8 2xl:py-16">
       <div className=" flex  mx-auto px-8 xl: ">
         <div className="text-white ">
           <h4 className="text-2xl md:text-4xl lg:text-7xl xl:text-8xl text-[#52eeca] md:mb-4 lg:mb-2">
