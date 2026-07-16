d63d29a46e7d0ab524ca2f4912aca2cfdf02ce43
tarikbouari <tarikbouari@gmail.com>
2026-07-16 19:58:30 +0200

    REFACTOR

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
     const data = await response.json();
 
     if (data.success) {
-      setResult('Message sent successfully ✅');
+      setResult('Message sent successfully');
       event.target.reset();
     } else {
       console.log('Error', data);
